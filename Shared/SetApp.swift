a2b8cb2d7ebc74f5efcddfdacb58762ba40d2e73
Maria Roodnitsky <maria.roodnitsky@gmail.com>
2022-06-03 08:38:44 -0700

    Initial Commit

---
+++ Shared/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Shared
+//
+//  Created by Maria Roodnitsky on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
